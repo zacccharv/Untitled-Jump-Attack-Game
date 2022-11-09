bd4e90df7fea7fc4581e7d478a74f65053dd0a89
Zaccharie Charvolin <zacchien1@gmail.com>
2022-11-09 01:53:57 -0500

    11-8-22

--- Assets/Scripts/Capabilities/Jump.cs
+++ Assets/Scripts/Capabilities/Jump.cs
@@ -4,56 +4,50 @@ using ZaccCharv;
 [RequireComponent(typeof(Controller))]
 public class Jump : MonoBehaviour
 {
+    #region Jump Class Only Vars
+
     [SerializeField, Range(0f, 10f)] private float _jumpHeight = 3f;
     [SerializeField, Range(0, 5)] public int _maxAirJumps = 0;
     [SerializeField, Range(0f, 5f)] public float _downwardMovementMultiplier = 3f;
     [SerializeField, Range(0f, 5f)] private float _upwardMovementMultiplier = 1.7f;
-
-    [SerializeField] private Controller _controller = null;
-    [SerializeField] private Move _move;
 
     private Animator _animator;
     private Rigidbody2D _body;
     private Ground _ground;
     private Vector2 _velocity;
 
-    public int _jumpPhase;
+    [SerializeField] private Controller _controller = null;
+
     private float _defaultGravityScale, _jumpSpeed;
-    private Vector2 _normal;
 
-    public bool _desiredJump, _onGround, leftWallHit, rightWallhit;
+    #endregion
 
-    // Start is called before the first frame update
-    void Awake()
+
+    public bool _desiredJump, _onGround;
+
+    public int _jumpPhase;
+
+    #region Wall Jumping and Sliding Vars
+
+    public bool _leftWallHit, _rightWallhit;
+    private bool _wallSliding;
+    private bool _wallJumping;
+    public float _wallJumpTime;
+
+    #endregion
+
+    void Start()
     {
         _body = GetComponent<Rigidbody2D>();
         _ground = GetComponent<Ground>();
         _controller = GetComponent<Controller>();
         _animator = GetComponent<Animator>();
-        _move = GetComponent<Move>();
 
         _defaultGravityScale = 1f;
     }
 
-
-    public float timeRemaining;
-    private bool timerIsRunning = false;
-    // Update is called once per frame
     void Update()
     {
-        if (timerIsRunning)
-        {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= .1f;
-            }
-            else
-            {
-                timerIsRunning = false;
-                Debug.Log("Time has run out!");
-            }
-        }
-        
         _desiredJump |= _controller.input.RetrieveJumpInput();
     }
 
@@ -62,35 +56,33 @@ public class Jump : MonoBehaviour
         _onGround = _ground.OnGround;
         _velocity = _body.velocity;
 
-        if ((rightWallhit || leftWallHit) && _body.velocity.y < 0 && !Input.GetButton("Jump"))
+        JumpActionCheck();
+
+        WallHitCheck();
+
+        WallSlidingCheck();
+
+        JumpVelocityCheck();
+
+        if (_wallJumping == true)
         {
-            if (rightWallhit)
-            {
-                Vector2 _wallJumpVel = new Vector2(-100, _velocity.y);
-                _body.velocity = _wallJumpVel;
-            }
-            else
-            {
-                _body.gravityScale = _downwardMovementMultiplier / 10;
-                int _newJumpPhase = Mathf.Max(_jumpPhase, 0);
-                _jumpPhase -= _newJumpPhase;
-            }
+            WallJumpAction();
+            Invoke("NotWallJumping", _wallJumpTime);
+        }
+        else
+        {
+            // This has to go last
+            _body.velocity = _velocity;
         }
 
-        if (_onGround)
-        {
-            _jumpPhase = 0;
-        }
-        if (_desiredJump)
-        {
-            _desiredJump = false;
-            JumpAction();
-        }
+    }
+    private void JumpVelocityCheck()
+    {
         if (_body.velocity.y > 0)
         {
             _body.gravityScale = _upwardMovementMultiplier;
         }
-        if (((_body.velocity.y < 0 || !Input.GetButton("Jump")) && !((rightWallhit || leftWallHit))) && timerIsRunning == false)
+        if (((_body.velocity.y < 0 || !_controller.input.RetrieveJumpInput()) && !(_wallSliding)) || _wallJumping == false)
         {
             _body.gravityScale = _downwardMovementMultiplier + .5f;
         }
@@ -98,48 +90,19 @@ public class Jump : MonoBehaviour
         {
             _body.gravityScale = _defaultGravityScale;
         }
-
-        /////////////////////////////////////////////////////////////////// HEREHEREHERE
-        if (timerIsRunning == true)
-        {
-            int _direction = 1;
-
-            if (leftWallHit)
-            {
-                _direction = 1;
-            }
-            if (rightWallhit)
-            {
-                _direction = -1;
-            }
-                Vector2 _wallJumpVel = new Vector2(25 * _direction, _velocity.y + 10);
-                _body.velocity = _wallJumpVel;
-                _body.gravityScale = _downwardMovementMultiplier + .5f;
-            if ((rightWallhit || leftWallHit))
-            {
-                timerIsRunning = false;
-            }
-        }
-        else
-        {
-            _body.velocity = _velocity;
-        }
-        /////////////////////////////////////////////////////////////////////
-
-        SidesRaycast();
     }
 
     private void JumpAction()
     {
-        if (_onGround || _jumpPhase < _maxAirJumps && !(rightWallhit || leftWallHit))
+        if (_onGround || _jumpPhase < _maxAirJumps && !(_rightWallhit || _leftWallHit))
         {
             _jumpPhase += 1;
 
             _animator.SetTrigger("Jumped");
             _animator.SetBool("Falling", false);
-                
+
             _jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * _jumpHeight);
-                
+
             if (_velocity.y > 0f)
             {
                 _jumpSpeed = Mathf.Max(_jumpSpeed - _velocity.y, 0f);
@@ -150,7 +113,7 @@ public class Jump : MonoBehaviour
             }
             _velocity.y += _jumpSpeed;
         }
-        if (rightWallhit || leftWallHit && _body.velocity.y < 0)
+        if (_wallSliding && _body.velocity.y < 0)
         {
             _jumpPhase += 2;
 
@@ -158,24 +121,30 @@ public class Jump : MonoBehaviour
             _animator.SetBool("Falling", false);
             _body.gravityScale = _downwardMovementMultiplier + .5f;
 
-            if (rightWallhit)
+            if (_wallSliding)
             {
-                timeRemaining = .4f;
-                timerIsRunning = true;
-                Debug.Log("I jumped off a wall");
-            }
-            if (leftWallHit)
-            {
-                timeRemaining = .4f;
-                timerIsRunning = true;
+                _wallJumping = true;
                 Debug.Log("I jumped off a wall");
             }
         }
     }
 
-    private void SidesRaycast()
+    private void JumpActionCheck()
     {
-        // Bit shift the index of the layer (8) to get a bit mask
+        if (_onGround)
+        {
+            _jumpPhase = 0;
+        }
+        if (_desiredJump)
+        {
+            _desiredJump = false;
+            JumpAction();
+        }
+    }
+
+    private void WallHitCheck()
+    {
+        // Bit shift the index of the layer (7) to get a bit mask
         int layerMask = 1 << 7;
 
         // This would cast rays only against colliders in layer 7.
@@ -189,21 +158,58 @@ public class Jump : MonoBehaviour
         {
             Debug.DrawLine(transform.position, transform.TransformDirection(Vector2.right), Color.yellow);
 
-            rightWallhit = true;
+            _rightWallhit = true;
         }
         else if (LeftCast && (!UpCast && !DownCast))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.left), Color.yellow);
 
-            leftWallHit = true;
+            _leftWallHit = true;
         }
         else
         {
-            leftWallHit = false; 
-            rightWallhit = false;
+            _leftWallHit = false;
+            _rightWallhit = false;
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.right) * 1000, Color.white);
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.left) * 1000, Color.white);
         }
     }
-}
 
+    private void WallSlidingCheck()
+    {
+        if (_rightWallhit || _leftWallHit)
+        {
+            _wallSliding = true;
+        }
+        else
+        {
+            _wallSliding = false;
+        }
+        if (_wallSliding && !_controller.input.RetrieveJumpInput())
+        {
+            _body.gravityScale = _downwardMovementMultiplier / 10;
+            int _newJumpPhase = Mathf.Max(_jumpPhase, 0);
+            _jumpPhase -= _newJumpPhase;
+        }
+    }
+
+    private void WallJumpAction()
+    {
+        float _direction = _controller.input.RetrieveMoveInput() * -1;
+        _body.gravityScale = _downwardMovementMultiplier + .5f;
+
+        if (_rightWallhit || _leftWallHit)
+        {
+            _wallJumping = false;
+        }
+
+        Vector2 _wallJumpVelocity = new Vector2(15 * _direction, 30);
+        _body.velocity = _wallJumpVelocity;
+    }
+
+    private void NotWallJumping()
+    {
+        _wallJumping = false;
+    }
+
+}
